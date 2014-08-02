69fece8d21c5198f51cb24e3f2606201573631c1
Hanspeter Salvisberg <hanspeter.salvisberg@gmx.ch>
2014-08-02 16:12:19 +0200

    Initial commit

---
+++ ue_config.php
@@ -0,0 +1,96 @@
+<?php
+$ueConfig['name_style']='2';
+$ueConfig['name_format']='3';
+$ueConfig['date_format']='m/d/Y';
+$ueConfig['calendar_type']='2';
+$ueConfig['allow_email_display']='2';
+$ueConfig['allow_email_replyto']='1';
+$ueConfig['allow_email']='1';
+$ueConfig['allow_website']='1';
+$ueConfig['allow_onlinestatus']='0';
+$ueConfig['icons_display']='7';
+$ueConfig['login_type']='1';
+$ueConfig['reg_admin_allowcbregistration']='0';
+$ueConfig['emailpass']='0';
+$ueConfig['reg_admin_approval']='1';
+$ueConfig['reg_confirmation']='1';
+$ueConfig['reg_username_checker']='0';
+$ueConfig['reg_email_checker']='0';
+$ueConfig['reg_show_login_on_page']='0';
+$ueConfig['reg_email_name']='User Registration SRM';
+$ueConfig['reg_email_from']='praesident@srm-murten.ch';
+$ueConfig['reg_email_replyto']='praesident@srm-murten.ch';
+$ueConfig['reg_pend_appr_sub']='Deine Registrierung wartet auf Deine Bestätigung';
+$ueConfig['reg_pend_appr_msg']='Hallo [NAME],
+Danke für deine Registrierungsanfrage auf der Homepage des SRM. 
+Die Anfrage wird bei uns bearbeitet, sobald du durch den Klick anf den nachfolgenden Link deine eMail-Adresse bestätigt hast.
+[CONFIRM]
+Sobald die eMail Adresse bestätigt ist, wird einer unserer Moderatoren die Zugriffsrechte vergeben und deine Freischaltung per Mail bestätigen.
+[DETAILS]
+Mit freundlichen Grüssen
+Das SRM-Website Admin Team';
+$ueConfig['reg_welcome_sub']='Nutzerdaten auf der SRM-Homepage';
+$ueConfig['reg_welcome_msg']='Willkommen [NAME]
+Dein Login wurde durch das Webteam SRM freigeschaltet und die für dich geltenden Zugriffsrechte vergeben.
+Dein Account hat die folgenden Details:
+[DETAILS]
+Wir wünschen dir viel Spass insbesodere im geschützten Bereich unserer Homepage, wo du alles Wissenswerte über Trainings und Wettkämpfe findest.
+Mit freundlichen Grüssen
+Das SRM-Website Admin Team';
+$ueConfig['reg_show_icons_explain']='3';
+$ueConfig['reg_intro_msg']='';
+$ueConfig['reg_conclusion_msg']='';
+$ueConfig['reg_enable_toc']='0';
+$ueConfig['reg_toc_url']='index.php?option=com_content&task=view&id=18';
+$ueConfig['reg_first_visit_url']='index.php?option=com_comprofiler';
+$ueConfig['num_per_page']='30';
+$ueConfig['allow_profilelink']='1';
+$ueConfig['usernameedit']='1';
+$ueConfig['adminrequiredfields']='0';
+$ueConfig['allow_profileviewbyGID']='-1';
+$ueConfig['maxEmailsPerHr']='10';
+$ueConfig['minHitsInterval']='60';
+$ueConfig['templatedir']='default';
+$ueConfig['left2colsWidth']='50';
+$ueConfig['left3colsWidth']='32';
+$ueConfig['right3colsWidth']='32';
+$ueConfig['showEmptyTabs']='0';
+$ueConfig['showEmptyFields']='0';
+$ueConfig['emptyFieldsText']='-';
+$ueConfig['nesttabs']='0';
+$ueConfig['xhtmlComply']='1';
+$ueConfig['frontend_userparams']='1';
+$ueConfig['use_divs']='0';
+$ueConfig['html_filter_allowed_tags']='';
+$ueConfig['im_path']='auto';
+$ueConfig['netpbm_path']='auto';
+$ueConfig['conversiontype']='4';
+$ueConfig['allowAvatar']='1';
+$ueConfig['allowAvatarUpload']='1';
+$ueConfig['avatarResizeAlways']='0';
+$ueConfig['allowAvatarGallery']='0';
+$ueConfig['avatarHeight']='500';
+$ueConfig['avatarWidth']='200';
+$ueConfig['avatarSize']='2000';
+$ueConfig['thumbHeight']='86';
+$ueConfig['thumbWidth']='60';
+$ueConfig['imageApproverGid']='17';
+$ueConfig['allowModUserApproval']='1';
+$ueConfig['moderatorEmail']='1';
+$ueConfig['allowUserReports']='0';
+$ueConfig['avatarUploadApproval']='0';
+$ueConfig['allowModeratorsUserEdit']='1';
+$ueConfig['allowUserBanning']='1';
+$ueConfig['allowConnections']='0';
+$ueConfig['connectionDisplay']='0';
+$ueConfig['connectionPath']='1';
+$ueConfig['useMutualConnections']='1';
+$ueConfig['conNotifyType']='0';
+$ueConfig['autoAddConnections']='1';
+$ueConfig['connection_categories']='Friend
+Co Worker';
+$ueConfig['translations_debug']='0';
+$ueConfig['enableSpoofCheck']='1';
+$ueConfig['noVersionCheck']='0';
+$ueConfig['version']='1.9';
+?>
